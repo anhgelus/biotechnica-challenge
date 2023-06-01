c537188c09fec27bfe57df7939a46d09baef1764
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2023-06-01 18:42:16 +0200

    ci(docker): remove mods, config and global_packs before installation

--- docker/start.sh
+++ docker/start.sh
@@ -1,13 +1,11 @@
-
 MODPACK_LINK=$1
 EULA=$2
 MEMORY=$3
 
-echo $MEMORY EULA=$EULA
+rm -fr /data/mods
+rm -fr /data/config
+rm -fr /data/global_packs
 
-cat /data/eula.txt
-
-echo $MODPACK_LINK && /app/mrpack-install $MODPACK_LINK --server-dir /data
 touch /data/eula.txt && echo eula=$EULA | tee /data/eula.txt
 cd /data && java -Xmx$MEMORY -Xms2G -jar quilt-server-launch.jar nogui
 
